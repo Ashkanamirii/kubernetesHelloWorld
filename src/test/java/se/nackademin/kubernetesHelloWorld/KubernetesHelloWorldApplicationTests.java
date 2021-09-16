4dad532abc10cec228cf1ec26c5c82550560c3c2
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-16 13:51:44 +0200

    Initial commit

---
+++ src/test/java/se/nackademin/kubernetesHelloWorld/KubernetesHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package se.nackademin.kubernetesHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
